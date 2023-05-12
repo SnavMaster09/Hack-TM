a48ad947d910d431b3bb82951122973c5492d210
MihneaTudor <mihneas.tudor@gmail.com>
2023-05-12 20:08:10 +0300

    UI v1

---
+++ Assets/Scenes/Scripts/ExitAppi.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{ 
+    public void ExitNow()
+    {
+        Debug.Log("Game is exiting");
+       // Application.Quit();
+       
+
+    }
+}
