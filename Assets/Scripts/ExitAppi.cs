ead8671ee016dbe17acc157ea6fd96f7df9efcd0
MihneaTudor <mihneas.tudor@gmail.com>
2023-05-12 20:19:36 +0300

    UI v1 rereUploeaded

--- Assets/Scripts/ExitAppi.cs
+++ Assets/Scripts/ExitAppi.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitApp : MonoBehaviour
+public class ExitAppi : MonoBehaviour
 { 
     public void ExitNow()
     {
