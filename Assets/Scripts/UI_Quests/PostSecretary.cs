400470c279d10486f09a3b112b4ab044e50edb1c
MihneaTudor <mihneas.tudor@gmail.com>
2023-05-13 12:57:24 +0300

    Quest v2

---
+++ Assets/Scripts/UI_Quests/PostSecretary.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PostSecretary : MonoBehaviour
+{
+    public TMP_Text My_Text;
+    private void OnCollisionEnter2D(Collider2D collision)
+    {
+        
+        Debug.Log(1);
+        
+    }
+}
