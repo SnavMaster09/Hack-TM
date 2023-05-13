ccaeaa6f5950ca47f32d02fa29b5ae01d260b6cb
MihneaTudor <mihneas.tudor@gmail.com>
2023-05-13 11:44:00 +0300

    UI_Quests-v1

--- Assets/Scripts/Minigame_Robot/Minigame.cs
+++ Assets/Scripts/Minigame_Robot/Minigame.cs
@@ -9,19 +9,149 @@ public class Minigame : MonoBehaviour
 {   char Lit;
     int cnt = 0;
     public TMP_Text MP_Text;
+    public  bool Pressed;
     void Start()
     {
-        InvokeRepeating("Repuri", 1, 3);
-
+        InvokeRepeating("Repuri", 0, 3);
+        
     } void Repuri()
     {
-        Lit = MP_Text.text[10];
-        /*if (Input.GetKey(Keycode.Lit))
-            cnt = cnt + 1;
-          */  
+        Lit = MP_Text.text[6];
+        Pressed = false;
     }
     void Update()
     {
+        if ((Input.GetKeyDown(KeyCode.A)) && Lit == 'A' && Pressed==false)
+        { 
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.B)) && Lit == 'B')
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.C)) && Lit == 'C' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.D)) && Lit == 'D' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.E)) && Lit == 'E' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.F)) && Lit == 'F' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.G)) && Lit == 'G' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.H)) && Lit == 'H' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.I)) && Lit == 'I' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.J)) && Lit == 'J' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.K)) && Lit == 'K' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.L)) && Lit == 'L' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.M)) && Lit == 'M' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.N)) && Lit == 'N' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.O)) && Lit == 'O' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.P)) && Lit == 'P' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.Q)) && Lit == 'Q' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.R)) && Lit == 'R' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.S)) && Lit == 'S' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.T)) && Lit == 'T' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.U)) && Lit == 'U' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.V)) && Lit == 'V' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.W)) && Lit == 'W' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.X)) && Lit == 'X' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.Y)) && Lit == 'Y' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.Z)) && Lit == 'Z' && Pressed == false)
+        {
+            cnt = cnt + 1;
+            Pressed = true;
+        }
+
         if (cnt == 5)
             SceneManager.LoadScene("Park");
     }
--- Assets/Scripts/Minigame_Robot/RandomLetter.cs
+++ Assets/Scripts/Minigame_Robot/RandomLetter.cs
@@ -15,33 +15,34 @@ public class RandomLetter : MonoBehaviour
     }
     public void Letter()
         {
+       
         rNum = Random.Range(1, 27);
-        if (rNum == 1) My_Text.text = "Hold down A";
-        if (rNum == 2) My_Text.text = "Hold down B";
-        if (rNum == 3) My_Text.text = "Hold down C";
-        if (rNum == 4) My_Text.text = "Hold down D";
-        if (rNum == 5) My_Text.text = "Hold down E";
-        if (rNum == 6) My_Text.text = "Hold down F";
-        if (rNum == 7) My_Text.text = "Hold down G";
-        if (rNum == 8) My_Text.text = "Hold down H";
-        if (rNum == 9) My_Text.text = "Hold down I";
-        if (rNum == 10) My_Text.text = "Hold down J";
-        if (rNum == 11) My_Text.text = "Hold down K";
-        if (rNum == 12) My_Text.text = "Hold down L";
-        if (rNum == 13) My_Text.text = "Hold down M";
-        if (rNum == 14) My_Text.text = "Hold down N";
-        if (rNum == 15) My_Text.text = "Hold down O";
-        if (rNum == 16) My_Text.text = "Hold down P";
-        if (rNum == 17) My_Text.text = "Hold down Q";
-        if (rNum == 18) My_Text.text = "Hold down R";
-        if (rNum == 19) My_Text.text = "Hold down S";
-        if (rNum == 20) My_Text.text = "Hold down T";
-        if (rNum == 21) My_Text.text = "Hold down U";
-        if (rNum == 22) My_Text.text = "Hold down V";
-        if (rNum == 23) My_Text.text = "Hold down W";
-        if (rNum == 24) My_Text.text = "Hold down X";
-        if (rNum == 25) My_Text.text = "Hold down Y";
-        if (rNum == 26) My_Text.text = "Hold down Z";
-    }
+        if (rNum == 1) My_Text.text = "Press A";
+        if (rNum == 2) My_Text.text = "Press B";
+        if (rNum == 3) My_Text.text = "Press C";
+        if (rNum == 4) My_Text.text = "Press D";
+        if (rNum == 5) My_Text.text = "Press E";
+        if (rNum == 6) My_Text.text = "Press F";
+        if (rNum == 7) My_Text.text = "Press G";
+        if (rNum == 8) My_Text.text = "Press H";
+        if (rNum == 9) My_Text.text = "Press I";
+        if (rNum == 10) My_Text.text = "Press J";
+        if (rNum == 11) My_Text.text = "Press K";
+        if (rNum == 12) My_Text.text = "Press L";
+        if (rNum == 13) My_Text.text = "Press M";
+        if (rNum == 14) My_Text.text = "Press N";
+        if (rNum == 15) My_Text.text = "Press O";
+        if (rNum == 16) My_Text.text = "Press P";
+        if (rNum == 17) My_Text.text = "Press Q";
+        if (rNum == 18) My_Text.text = "Press R";
+        if (rNum == 19) My_Text.text = "Press S";
+        if (rNum == 20) My_Text.text = "Press T";
+        if (rNum == 21) My_Text.text = "Press U";
+        if (rNum == 22) My_Text.text = "Press V";
+        if (rNum == 23) My_Text.text = "Press W";
+        if (rNum == 24) My_Text.text = "Press X";
+        if (rNum == 25) My_Text.text = "Press Y";
+        if (rNum == 26) My_Text.text = "Press Z";
+    }   
     }
 
